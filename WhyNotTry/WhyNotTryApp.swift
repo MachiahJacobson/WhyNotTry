f5afa351a41b33a541a89251bb8f76295694c7a2
Mac <macjacobson99@gmail.com>
2025-10-14 15:08:08 -0600

    Initial Commit

---
+++ WhyNotTry/WhyNotTryApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhyNotTryApp.swift
+//  WhyNotTry
+//
+//  Created by Jacobson, Machiah - Student on 10/14/25.
+//
+
+import SwiftUI
+
+@main
+struct WhyNotTryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WhyNotTryTests/WhyNotTryTests.swift
@@ -0,0 +1,17 @@
+//
+//  WhyNotTryTests.swift
+//  WhyNotTryTests
+//
+//  Created by Jacobson, Machiah - Student on 10/14/25.
+//
+
+import Testing
+@testable import WhyNotTry
+
+struct WhyNotTryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
