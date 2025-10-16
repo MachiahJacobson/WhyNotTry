5aea8d491073c7cb631db7fed104af7568f07be7
Mac <macjacobson99@gmail.com>
2025-10-15 23:04:22 -0600

    Final project of the Rock, Paper, Scissors challenge

--- WhyNotTry/ContentView.swift
+++ WhyNotTry/ContentView.swift
@@ -8,14 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var selected = "Baseball"
+    var activities = ["Archery", "Hiking", "Cooking", "Reading", "Board Games"]
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Why not try...")
+                .font(.largeTitle.bold())
+            VStack {
+                Circle()
+                    .fill(.blue)
+                    .padding()
+                    .overlay(
+                        Image(systemName: "figure.\(selected.lowercased())")
+                            .font(.system(size: 144))
+                            .foregroundColor(.white)
+                    )
+                Text("\(selected)!")
+                    .font(.title)
+            }
+            Spacer()
+            Button("Try again") {
+                selected = activities.randomElement() ?? "Baseball"
+            }
+            .buttonStyle(.borderedProminent)
         }
-        .padding()
     }
 }
 
